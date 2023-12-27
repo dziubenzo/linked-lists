510bf43df64c34471a3d923fecbae81e7a1a8d6f
dziubenzo <dziubenzo@gmail.com>
2023-12-27 11:41:07 +0100

    Build append() method

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 class LinkedList {
-  static list = {};
+  constructor(node) {
+    this.value = node.value;
+    this.next = node.next;
+  }
+
+  append(node) {
+    const addToEnd = (node, list = this) => {
+      if (list.next === null) {
+        list.next = node;
+        return;
+      } else {
+        addToEnd(node, list.next);
+      }
+    };
+    addToEnd(node);
+  }
+
+  prepend(node) {}
 }
 
 class Node {
@@ -8,3 +25,12 @@ class Node {
     this.next = next;
   }
 }
+
+let linkedList = new LinkedList(new Node(5));
+linkedList.append(new Node(10));
+linkedList.append(new Node(15));
+linkedList.append(new Node(20));
+linkedList.append(new Node(25));
+linkedList.append(new Node(30));
+linkedList.prepend(new Node(1));
+console.log(linkedList);
